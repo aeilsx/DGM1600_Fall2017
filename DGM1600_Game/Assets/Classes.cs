0e1ec7f6e145825db1f693dacd79f69985d9607d
Unknown <lexi.hougaard@gmail.com>
2017-08-30 20:29:52 -0600

    COMMITTING MY ENTIRE REPOSITORY
    I was lost and now I'm found!

---
+++ DGM1600_Game/Assets/Classes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+	public class One {
+		public int money = 1000000;
+		public float fuel = 0.01f;
+
+		private string name = "Bob";
+
+	}
+
+	public class Two {
+		
+		public int food = -1000000;
+		private float knives = 0.02f;
+		string complaint = "I'M REALLY HUNGRY!!!!!!!!!!!!!!!";
+
+	}
+	public class Three {
+		private int pizza = 1;
+		public float heart = 0;
+		public string announcement = "You have a pizza my heart.";
+		
+	}
+
+}
