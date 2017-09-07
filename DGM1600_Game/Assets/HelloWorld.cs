9ebc764005a2fefb405c35677e32031f90335090
Unknown <lexi.hougaard@gmail.com>
2017-09-06 20:18:48 -0600

    In-Class: Variables
    hello world redesigned for me understanding variables!!!!!!~!!!!!!

--- DGM1600_Game/Assets/HelloWorld.cs
+++ DGM1600_Game/Assets/HelloWorld.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		print ("Hello World!!");
-	}
+private int bottledWater = 5;
+//variable and value for how much water is in the bottled water
+//water bottle represents a container for values
+string waterBottle = "Full";
+//string message displaing that water bottle is full
+//another way to represent bottle fullness
+public int diseases = 1000;
+//variable and value for diseases
+//public and private (access modifiers) declaration determine varibale's accessibilty
+public float floaties = 0.16f;
+//floats are for numbers smaller than 1
+//the water is about 16 percent floaties
+public int owner = 5;
+//there are 5 owners
+//these can all be used to describe the bottled water's paths and circumstances
+
+
+// string message = "Help Me!!";
+
+// 	// Use this for initialization
+// 	void Start () {
+// 		print ("Hello World!!");
+// 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+// 	// Update is called once per frame
+// 	void Update () {
+//		print(message);		
+// 	}
 }
