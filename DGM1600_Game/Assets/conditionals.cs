a8bb8ce099aaa84195175e4800339261dbac08ba
Unknown <lexi.hougaard@gmail.com>
2017-09-11 20:21:21 -0600

    CONDITIONALS!!!

---
+++ DGM1600_Game/Assets/conditionals.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+ 
+public class conditionals : MonoBehaviour { 
+  //Conventions and syntax 
+  //each line of code needs to end with a ;  
+  //{} show the beginning and end of code 
+  //  
+ 
+  public string stopLight = "Red"; 
+  void start(){ 
+    Main(); 
+  } 
+  void Main () { 
+    if(stopLight == "Red") { 
+    print("STOP! ThE lIgHt Is ReD!!"); 
+ 
+    }  
+    else if (stopLight == "Yellow"){  
+    print("SlOw DoWn!!!"); 
+    } 
+    else if (stopLight == "Green"){  
+    print("Go!!!!"); 
+    } 
+    else{ 
+    print("I dOn'T kNoW wHaT yOu MeAn!"); 
+    } 
+ 
+ 
+} 
+ 
+}
