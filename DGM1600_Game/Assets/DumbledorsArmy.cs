8f0c1a9db8402e8b626fd126fe99b351b7d24776
Unknown <lexi.hougaard@gmail.com>
2017-10-11 20:25:33 -0600

    For Each Lists

---
+++ DGM1600_Game/Assets/DumbledorsArmy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DumbledorsArmy : MonoBehaviour {
+
+	public string name;
+		public string wand;
+
+		public DumbledorsArmy(string newName, string newWand){
+			name = newName;
+			wand = newWand;
+		}
+
+		public int CompareTo(DumbledorsArmy other)
+		{
+			if(other == null){
+				return 1;
+			}
+
+			return wand - other.wand;
+		}
+
+}
