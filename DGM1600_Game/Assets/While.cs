25553b026051ae110530f850bcc98a9fbfa384c3
Unknown <lexi.hougaard@gmail.com>
2017-10-23 23:57:31 -0600

    While loops

--- DGM1600_Game/Assets/While.cs
+++ DGM1600_Game/Assets/While.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//while loops test each condition against each other and executes the expresstion until it's proven false.
+//can be infinate or a certain amount of loops. It can be used to repeat actions until the conditions are fuffilled and can be used with 
+//for loops and more. Uses a counter usually. 
+
 public class While : MonoBehaviour {
 
 	public int bottles= 0;
@@ -11,11 +15,78 @@ public class While : MonoBehaviour {
 			bottles ++;
 			print(bottles + "bottles of beer on the wall!!");
 		}
-		
+
+	public int points = 0;
+	
+	//point counter
+	while(points < 25){
+		points ++:
+		print("UVU" + points);	
+	}
+
+	//countdown
+	public int time;
+
+	while(time > 0){
+		time --;
+		print(time);
+	}
+
+	//hits
+	public int health = 30;
+
+	while(health >= 1){
+		health --;
+		print("-1 Health");
+	}
+
+	//hunger
+	public int hunger = 0;
+
+	while(hunger < 100){
+		hunger ++;
+		print("Eat as much as you want!!");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	//seats available
+	public int seats = 60;
+
+	while(seats > 0){
+		seats --;
+		print(seats + "seats left!");
+	}
+
+	//baking
+	public int cups;
+
+	while(cups < 2){
+		cups ++;
+		print("Add a cup of flour.");
+	}
+
+	//homework
+	public int assignments;
+
+	while(assignments > 0){
+		assignments --;
+		print("One down " + assignments + "left to go.");
+	}
+
+	//eneimes
+	public int enemy = 20;
+
+	while(enemy >= 0){
+		enemy --;
+		print(enemy + "Enemies remain.");
+	}
+
+	//friends
+	public int friend = 0;
+
+	while(friend < 80){
+		friend ++;
+		print("I have" + friend + "friends!!");
+	}
+
 	}
 }
